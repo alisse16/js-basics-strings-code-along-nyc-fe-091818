b934d9bd31ef0675c1a798f3762f3d7ef6f96794
Alisse Frandsen <alisse.frandsen@gmail.com>
2018-10-17 00:48:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ let currentUser = "alisse"
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
-let shortGreeting = "Welcome, " + currentUser.charAt(0) 
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!"
